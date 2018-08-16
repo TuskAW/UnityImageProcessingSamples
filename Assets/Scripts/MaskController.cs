7b8d64a6119e0ed7c41069cc7d49d5e714ebaf51
sotanmochi <stmczk@gmail.com>
2018-08-16 13:20:40 +0900

    Update.

---
+++ Assets/Scripts/MaskController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskController : MonoBehaviour
+{	
+	void Update()
+	{
+		transform.Translate(0.01f*Input.GetAxis("Horizontal"), 0.01f*Input.GetAxis("Vertical"), 0.0f);
+	}
+}
