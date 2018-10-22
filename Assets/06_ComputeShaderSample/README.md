24ab516822d1b35220e6a103d8c16c43e3c10e54
sotanmochi <stmczk@gmail.com>
2018-10-22 12:46:01 +0900

    Add compute shader sample

---
+++ Assets/06_ComputeShaderSample/README.md
@@ -0,0 +1,10 @@
+# Compute Shader Sample
+
+Compute Shaderでソーベルフィルタを実装
+
+<img src="./ComputeShaderSample.png" width="50%">
+
+## References
+- [Unity Graphics Programming vol.1](https://indievisuallab.stores.jp/items/59edf11ac8f22c0152002588)
+- [UnityGraphicsProgramming](https://github.com/IndieVisualLab/UnityGraphicsProgramming)
+- [Unity5ComputeShaderTest](https://github.com/shaderjp/Unity5ComputeShaderTest)
---
+++ Assets/06_ComputeShaderSample/SobelFilteringComputeShader.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SobelFilteringComputeShader : MonoBehaviour
+{
+    public ComputeShader computeShader;
+	public Material inputImageMaterial;
+	public Material outputImageMaterial;
+
+	private Texture srcTexture;
+	private RenderTexture dstTexture;
+
+	void Start()
+	{
+		srcTexture = inputImageMaterial.mainTexture;
+        dstTexture = new RenderTexture(srcTexture.width, srcTexture.height, 0, RenderTextureFormat.ARGB32);
+        dstTexture.enableRandomWrite = true;
+        dstTexture.Create();
+	}
+
+	void Update()
+	{
+        if(!SystemInfo.supportsComputeShaders)
+        {
+			Debug.LogError("Compute Shader is not Support!!");
+			return;
+        }
+		if(computeShader == null)
+		{
+			Debug.LogError("Compute Shader has not been assigned!!");
+			return;
+		}
+
+		int kernelID = computeShader.FindKernel("SobelFilterCS");
+		computeShader.SetTexture(kernelID, "srcTexture", srcTexture);
+        computeShader.SetTexture(kernelID, "Result", dstTexture);
+        computeShader.Dispatch(kernelID, srcTexture.width/8, srcTexture.height/8, 1);
+
+		outputImageMaterial.mainTexture = dstTexture;
+	}
+}
