445ee2b6069193568ddb292eeeca0485f45b7422
sotanmochi <stmczk@gmail.com>
2019-04-21 14:25:48 +0900

    Add sample applying compute shader to the camera view

---
+++ Assets/07_ApplyComputeShaderToCameraView/README.md
@@ -0,0 +1,8 @@
+# Apply Compute Shader to Camera View
+
+Compute Shaderでソーベルフィルタを実装
+
+## References
+- [Unity Graphics Programming vol.1](https://indievisuallab.stores.jp/items/59edf11ac8f22c0152002588)
+- [UnityGraphicsProgramming](https://github.com/IndieVisualLab/UnityGraphicsProgramming)
+- [Unity5ComputeShaderTest](https://github.com/shaderjp/Unity5ComputeShaderTest)
---
+++ Assets/07_ApplyComputeShaderToCameraView/Scripts/ApplyComputeShaderToCameraView.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(Camera))]
+public class ApplyComputeShaderToCameraView : MonoBehaviour
+{
+    [SerializeField]
+	ComputeShader _computeShader;
+
+	Vector2Int _gpuThreads = new Vector2Int(16, 16);
+
+	RenderTexture _src;
+	RenderTexture _target;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dst)
+	{
+		_src = src;
+
+		InitializeRenderTexture();
+		DispatchComputeShader();
+
+		Graphics.Blit(_target, dst);
+	}
+
+	void InitializeRenderTexture()
+	{
+		if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_target != null){
+	            _target.Release();
+			}
+
+            // Get a render target
+            _target = new RenderTexture(Screen.width, Screen.height, 0);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+	}
+
+	void DispatchComputeShader()
+	{
+        if(!SystemInfo.supportsComputeShaders)
+        {
+			Debug.LogError("Compute Shader is not Support!!");
+			return;
+        }
+		if(_computeShader == null)
+		{
+			Debug.LogError("Compute Shader has not been assigned!!");
+			return;
+		}
+
+		int kernelID = _computeShader.FindKernel("SobelFilterCS");
+		_computeShader.SetTexture(kernelID, "_SrcImage", _src);
+        _computeShader.SetTexture(kernelID, "_Result", _target);
+        _computeShader.Dispatch(kernelID, Mathf.CeilToInt((float)_target.width / _gpuThreads.x), 
+										  Mathf.CeilToInt((float)_target.height / _gpuThreads.y), 1);
+	}
+}
